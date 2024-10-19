7aebc8cc3e1e53cf3a1b66131f433724b047fe8d
Magne <lsj2015@hotmail.com>
2024-10-19 11:43:09 +0800

    Initial Commit

---
+++ SwiftfulSourceControll/SwiftfulSourceControllApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControllApp.swift
+//  SwiftfulSourceControll
+//
+//  Created by Sean Lee on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
