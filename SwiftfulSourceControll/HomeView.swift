35a74d121db287852f8c7137d757704c3194ff34
Magne <lsj2015@hotmail.com>
2024-10-20 21:11:52 +0800

    [Feature] Delete extra texts

--- SwiftfulSourceControll/HomeView.swift
+++ SwiftfulSourceControll/HomeView.swift
@@ -14,9 +14,7 @@ struct HomeView: View {
     VStack {
       Text("Hi")
       Text("Screen 2!")
-      
-      Text("Screen 2!")
-      Text("Screen 3!")
+
     }
     .onAppear {
       // send analytics
